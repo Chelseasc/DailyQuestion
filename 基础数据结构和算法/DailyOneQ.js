b7c8c280ac7185f2e0b2897f6fd7365a30b8c07c
沈忱 <sshencchen@sina.com>
2020-04-08 10:14:25 +0800

    14

--- 基础数据结构和算法/DailyOneQ.js
+++ 基础数据结构和算法/DailyOneQ.js
@@ -107,3 +107,19 @@ var romanToInt = function(s) {
         return result;
     }
 };
+
+// 14. 最长公共前缀
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0) return '';
+    if(strs.length === 1) return strs[0];
+    let result = '';
+    for(let i = 0; i < strs[0].length; i++) {
+        for(let j = 1; j < strs.length; j++) {
+            if(strs[0][i] !== strs[j][i]) {
+                return result;
+            }
+        }
+        result += strs[0][i];
+    }
+    return result;
+};
