188671a6ad720973ea334c946d3739a96b887619
沈忱 <sshencchen@sina.com>
2020-04-16 10:21:37 +0800

    35

--- 基础数据结构和算法/DailyOneQ.js
+++ 基础数据结构和算法/DailyOneQ.js
@@ -259,3 +259,17 @@ var strStr = function(haystack, needle) {
     }
     return -1;
   };
+
+//   35. 搜索插入位置
+// 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+// 你可以假设数组中无重复元素。
+var searchInsert = function(nums, target) {
+    const index = nums.indexOf(target);
+    if(index !== -1) return index;
+    for(let i = 0; i < nums.length; i++) {
+      if(target < nums[i]) {
+        return i;
+      }
+    }
+    return nums.length;
+  };
